69abe9f61807b063f46c6f77ee36492d97a20362
SnahashisShuvon <snahashis.shuvon.dev@gmail.com>
2023-07-04 22:23:30 +0600

    ProductController & Repository Pattern has been added

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+        public void Update(Product category)
+        {
+            _context.Products.Update(category);
+        }
+    }
+}
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -1,10 +1,5 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository
 {
@@ -12,14 +7,16 @@ namespace Bulky.DataAccess.Repository
     {
         private readonly ApplicationDbContext _context;
         public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
 
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
             Category = new CategoryRepository(_context);
+            Product = new ProductRepository(_context);
         }
 
-        
+
 
         public void Save()
         {
