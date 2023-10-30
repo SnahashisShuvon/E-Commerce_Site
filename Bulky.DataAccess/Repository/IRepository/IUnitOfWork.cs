f579e07694f470403b0d29e9ab3bc347db58ffb9
SnahashisShuvon <101804260+SnahashisShuvon@users.noreply.github.com>
2023-10-31 02:32:13 +0600

    Revert "Order Confirmation has been completed!"

--- Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -7,8 +7,6 @@
         ICompanyRepository Company { get; }
         IShoppingCartRepository ShoppingCart { get; }
         IApplicationUserRepository ApplicationUser { get; }
-        IOrderHeaderRepository OrderHeader { get; }
-        IOrderDetailRepository OrderDetail { get; }
 
         void Save();
     }
