f64cebe89b9c80ab0e33e937aa394dd46a5f8797
LaptopPER\S R Shuvon <snahashis.shuvon.dev@gmail.com>
2023-10-31 20:54:42 +0600

    Revert "Order Confirmation has been completed!"
    This reverts commit 1d0dc254e3035383c56a6fcc17bc3a95695f8c20.

--- Bulky.DataAccess/Repository/IRepository/IOrderDetailRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bulky.Models;
-
-namespace Bulky.DataAccess.Repository.IRepository
-{
-    public interface IOrderDetailRepository : IRepository<OrderDetail>
-    {
-        void Update(OrderDetail orderDetail);
-    }
-}
